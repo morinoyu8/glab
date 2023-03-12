82e83c2327681603f87aaea0886ef2bc5dc53262
kohei8281 <kohei8281@gmail.com>
2023-03-13 00:19:56 +0900

    edit main page

--- js/template.js
+++ js/template.js
@@ -1,6 +1,15 @@
-$("#header").load("../templete/header.html");
-$("#footer").load("../templete/footer.html");
-$("#jc").load("../main/journal-club.html");
-$("#it").load("../main/intro-thesis.html");
-$("#pr").load("../main/progress-report.html");
-$("#lt").load("../main/lt.html");
+if (location.href.indexOf("https://morinoyu8.github.io") < 0) {
+    $("#header").load("/templete/header.html");
+    $("#footer").load("/templete/footer.html");
+    $("#jc").load("/main/journal-club.html");
+    $("#it").load("/main/intro-thesis.html");
+    $("#pr").load("/main/progress-report.html");
+    $("#lt").load("/main/lt.html");
+} else {
+    $("#header").load("/glab/templete/header.html");
+    $("#footer").load("/glab/templete/footer.html");
+    $("#jc").load("/glab/main/journal-club.html");
+    $("#it").load("/glab/main/intro-thesis.html");
+    $("#pr").load("/glab/main/progress-report.html");
+    $("#lt").load("/glab/main/lt.html");
+}
