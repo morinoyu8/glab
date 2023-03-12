283b0b6b08c45c7f35af33be16f2e0bbe8c24355
kohei8281 <kohei8281@gmail.com>
2023-03-12 23:57:21 +0900

    edit main page

--- js/template.js
+++ js/template.js
@@ -1,3 +1,6 @@
 $("#header").load("/templete/header.html");
 $("#footer").load("/templete/footer.html");
 $("#jc").load("/main/journal-club.html");
+$("#it").load("/main/intro-thesis.html");
+$("#pr").load("/main/progress-report.html");
+$("#lt").load("/main/lt.html");
